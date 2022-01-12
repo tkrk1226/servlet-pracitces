bcde59807ae7218bc804edf0e0c03dd3bf0bf200
UijinJung <tkrk1226@ajou.ac.kr>
2022-01-12 17:46:50 +0900

    emaillist#3 - emaillistDao.findAll() 구현

---
+++ emaillist01/src/main/java/com/poscoict/emaillist/dao/EmaillistDao.java
@@ -0,0 +1,80 @@
+package com.poscoict.emaillist.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.poscoict.emaillist.vo.EmaillistVo;
+
+public class EmaillistDao {
+	public List<EmaillistVo> findAll(){		
+		
+		List<EmaillistVo> result = new ArrayList<>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			//1. JDBC 드라이버 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver"); // Java 6 버전부터는 생략 가능
+			
+			//2. 연결하기
+			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");						
+
+			//3. SQL 준비
+			String sql = "select no, first_name, last_name, email from emaillist order by no desc";
+			pstmt = conn.prepareStatement(sql);
+
+			//4. 바인딩(binding) setString...?
+			
+			//5. SQL 실행
+			rs = pstmt.executeQuery();
+			
+			//boiler plate code => 써야되긴 하는데 너무 뻔한 내용이 길어지는... => 비효율
+			
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				String email = rs.getString(4);
+			
+				EmaillistVo vo = new EmaillistVo();
+				vo.setNo(no);
+				vo.setFirstName(firstName);
+				vo.setLastName(lastName);
+				vo.setEmail(email);
+				
+				result.add(vo);
+			}
+			
+		} catch (ClassNotFoundException e) {
+			System.out.print("드라이버 로딩 실패 : " + e); // e.getMessage()
+		} catch (SQLException e) {
+			System.out.print("error : " + e); // e.getMessage()
+		}
+		
+		finally {
+			// 자원 정리
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+}
