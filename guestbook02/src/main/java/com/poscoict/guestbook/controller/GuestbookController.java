6ae96bca415486671d61f2209b59180e022c1e22
UijinJung <tkrk1226@ajou.ac.kr>
2022-01-13 17:43:30 +0900

    guestbook02 #1

---
+++ guestbook02/src/main/java/com/poscoict/guestbook/controller/GuestbookController.java
@@ -0,0 +1,25 @@
+package com.poscoict.guestbook.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GuestbookController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+		
+		String actionName = request.getParameter("a");
+		
+		response.getWriter().println("<h1>guest!!!!!!!!!!!</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
