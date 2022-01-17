edf35dcb7e9a82204fb7156ca6cb296c82b1226d
UijinJung <tkrk1226@ajou.ac.kr>
2022-01-17 14:56:29 +0900

    jstlel #2

--- jstlel/src/main/java/jstlel/_01Servlet.java
+++ jstlel/src/main/java/jstlel/_01Servlet.java
@@ -1,6 +1,9 @@
 package jstlel;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +14,39 @@ public class _01Servlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		// 값
+		int iVal = 10;
+		long lVal = 10;
+		float fVal = 3.14f;
+		boolean bVal = true;
+		String sVal = "가나다라마바사";
+		
+		
+		// 객체
+		Object obj = null;
+		UserVo userVo = new UserVo();
+		userVo.setNo(10L);
+		userVo.setName("둘리");
+		
+		// map
+		Map<String, Object> map = new HashMap<>();
+		map.put("ival", iVal); // Auto Boxing (iVal)
+		map.put("lval", lVal);
+		map.put("fval", fVal);
+		map.put("bval", bVal);
+		map.put("sval", sVal);		
+		
+		request.setAttribute("ival", iVal);
+		request.setAttribute("lval", lVal);
+		request.setAttribute("fval", fVal);
+		request.setAttribute("bval", bVal);
+		request.setAttribute("sval", sVal);
+
+		request.setAttribute("obj", obj);
+		request.setAttribute("user", userVo);
+		
+		request.setAttribute("m", map);
+		
 		request
 			.getRequestDispatcher("/WEB-INF/views/01.jsp")
 			.forward(request, response);
--- jstlel/src/main/webapp/WEB-INF/views/01.jsp
+++ jstlel/src/main/webapp/WEB-INF/views/01.jsp
@@ -8,5 +8,48 @@
 </head>
 <body>
 	<h1>EL 연습</h1>
+	
+	<h4>값 출력</h4>
+	${ requestScope.ival } <br/>
+	${ ival } <br/>
+	${ lval } <br/>
+	${ fval } <br/>
+	${ bval } <br/>
+	${ sval } <br/>
+	
+	<h4>객체 출력</h4>
+	---${ obj }---  <br/>
+	${ user.no } <br/>
+	${ user.name } <br/>
+	
+	<h4>map의 값 출력</h4>
+	${ m.ival } <br/>
+	${ m.lval } <br/>
+	${ m.fval } <br/>
+	${ m.bval } <br/>
+	${ m.sval } <br/>
+	
+	<h4>산술 연산</h4>
+	${ 3 * 4 + 6 / 2 } <br/>
+	${ ival + 10 } <br/>
+	
+	<h4>관계 연산</h4>
+	${ ival == 10 } <br/>
+	${ ival < 5 } <br/>
+	${ obj == null } <br/>
+	${ empty obj } <br/>
+	${ obj != null } <br/>
+	${not empty obj } <br/>
+	
+	<h4>논리 연산</h4>
+	${ ival == 10 && lval < 100 } <br/>
+	${ ival == 10 || lval < 100 } <br/>
+	
+	<h4>요청 파라미터</h4>
+	---${ param.a + 10 } <br/>
+	---${ param.email } <br/>
+	
+	
+		
 </body>
 </html>
